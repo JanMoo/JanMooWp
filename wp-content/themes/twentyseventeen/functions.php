ef785968c30e04c42db3d54207e041185530f644
JanMoo <32166427+JanMoo@users.noreply.github.com>
2019-08-21 13:10:59 +0200

    adding widget and content
    added widget to set my current state of employment
    and adding lots of content

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -662,3 +662,74 @@ require get_parent_theme_file_path( '/inc/customizer.php' );
  * SVG icons functions and filters.
  */
 require get_parent_theme_file_path( '/inc/icon-functions.php' );
+
+/** adding custom widget **/
+// Register and load the widget
+function wpb_load_widget() {
+    register_widget( 'employment_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
+
+// Creating the widget
+class employment_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+
+// Base ID of your widget
+'employment_widget',
+
+// Widget name will appear in UI
+__('state of employment Widget', 'wpb_widget_domain'),
+
+// Widget description
+array( 'description' => __( 'widget that sets current state of employment', 'wpb_widget_domain' ), )
+);
+}
+
+// Creating widget front-end
+
+public function widget( $args, $instance ) {
+$employer = apply_filters( 'widget_title', $instance['title'] );
+$title = 'current state of employment';
+
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+echo __( $employer, 'wpb_widget_domain' );
+echo $args['after_widget'];
+}
+
+// Widget Backend
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'wpb_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php
+}
+
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class wpb_widget ends here
+
+//line break shortcode [br]
+function add_linebreak_shortcode() {
+return '<br />';
+}
+add_shortcode('br', 'add_linebreak_shortcode' );
